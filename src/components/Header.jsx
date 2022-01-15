77ec935400cae9767128391afd229a7bd86303f5
Luis Gomez <luiskaco@gmail.com>
2022-01-15 03:02:06 -0500

    Actualizando

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header(props) {
         <>
             <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">Seguimiento Paciente
                 {' '} 
-                <span className="text-indigo-600 ">Verinaria</span>
+                <span className="text-indigo-600 ">Verinaria 2022</span>
             </h1>
         </>
      );
